ed38fbd6611125e6d0530b5492db7764f2d17945
DennisMcMorrow <mcmorrowdennis@gmail.com>
2023-09-23 15:23:39 -0400

    commit for untracked files

---
+++ src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <!-- Header Section -->
+        <header>
+            <h1>Jackson EMS</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>News</li>
+                    <li>Calendar</li>
+                    <li>Donate</li>
+                </ul>
+            </nav>
+        </header>
+
+        <!-- Main Content Section -->
+        <main>
+            <section id="news-feed">
+                <h2>Latest News</h2>
+                <!-- We will loop through news articles here in the future -->
+            </section>
+        </main>
+
+        <!-- Footer Section -->
+        <footer>
+            <p>© 2023 Jackson EMS</p>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      // We will declare our data properties here in the future
+    };
+  },
+  methods: {
+    // We will declare methods here in the future
+  }
+};
+</script>
+
+<style>
+    /* Add your styles here */
+</style>
