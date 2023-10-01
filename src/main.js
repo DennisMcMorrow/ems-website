09cc2b53521a2e7f513cdb4d3841ef05b979242f
DennisMcMorrow <mcmorrowdennis@gmail.com>
2023-10-01 16:46:17 -0400

    fixed dependency error and commented out firebase import and removed git

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faMoneyBill } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import router from './router';
-import firebase from "@/firebaseConfig.js";
+//import firebase from "@/firebaseConfig.js";
 
 library.add(faMoneyBill, faFacebook, faTwitter, faInstagram);
 
